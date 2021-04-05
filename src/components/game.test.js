d5842d02bd3e394e99be567890461b15cb726c38
Amol <mailmeamol2112@gmail.com>
2021-04-05 18:24:16 -0400

    JEST compatible version installation.

--- src/components/game.test.js
+++ src/components/game.test.js
@@ -1,6 +1,9 @@
 import React from "react";
 import Game from "./game";
 import { shallow, mount } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+import { configure } from "enzyme";
+configure({ adapter: new Adapter() });
 
 it("renders without crashing", () => {
   shallow(<Game />);
